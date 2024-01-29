e867eaabb2a541ce29f6f852468821d7b516e487
elmirahtm <elmirahatamova28@gmail.com>
2024-01-29 13:20:31 +0400

    update

--- components/HeaderRoutes/index.tsx
+++ components/HeaderRoutes/index.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const HeaderRoutes = () => {
 const basliq=[
-  {id:1, to:"/əsasSəhifə"  , title:"Əsas Səhifə"},
+  {id:1, to:"/əsassəhifə"  , title:"Əsas Səhifə"},
   {id:2, to:"/təmir"  , title:"Təmir"},
     {id:3, to:"/quraşdırma"  , title:"Quraşdırma"},
-    {id:4, to:"/ustaSifarişi"  , title:"Usta Sifarişi"},
-    {id:5, to:"/çatdırıma"  , title:"Çatdırılma"},
+    {id:4, to:"/ustasifarişi"  , title:"Usta Sifarişi"},
+    {id:5, to:"/çatdırılma"  , title:"Çatdırılma"},
     {id:6, to:"/zəmanət"  , title:"Zəmanət"},
     {id:7, to:"/haqqımızda"  , title:"Haqqımızda"},
     {id:8, to:"/əlaqə"  , title:"Əlaqə"}
