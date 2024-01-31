f2e5348d5283f9e7d28d1b3433109f42c45103c1
elmirahtm <elmirahatamova28@gmail.com>
2024-02-01 00:49:49 +0400

    index

--- components/BodyList2/index.tsx
+++ components/BodyList2/index.tsx
@@ -4,243 +4,166 @@ import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import Card from "../Card";
 import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
+import Card2 from "../Card2";
 
 const BodyList2 = () => {
   return (
     <div>
       <div>
-        <div>
-          <h1 className="text-center font-bold text-xl max-sm:w-72 max-sm:m-auto">
-            Bütün brendlərin məişət texnikasını təmir edirik
-          </h1>
-        </div>
-      </div>
-
-      <div>
-        <span className="flex gap-10">
-          <span className="max-sm:-mt-6">
-            <IoIosArrowBack />
-          </span>
-
-          <span>
-            <Image src="/beko.png" alt="beko" width={83} height={65} />
-          </span>
-          <span>
-            <Image src="/blomberg.png" alt="blomberg" width={144} height={65} />
-          </span>
-          <span>
-            <Image src="/grundig.png" alt="grundig" width={182} height={65} />
-          </span>
-          <span>
-            <Image src="/riffel.png" alt="riffel" width={126} height={65} />
-          </span>
-          <span className="mt-1.5 ">
-            <Image src="/lg.png" alt="lg" width={72} height={65} />
-          </span>
-          <span>
-            <Image src="/samsung.png" alt="samsung" width={146} height={65} />
-          </span>
-
-          <span className=" max-sm:-mt-6">
-            <IoIosArrowForward />
-          </span>
-        </span>
-      </div>
-
-      <div className='bg-[url("/bg.png")] w-full h-[20rem]'>
-        {/* <Image className='translate-y-36 ' src="/bg.png" alt='bg' width={1490} height={1190}/> */}
-
-        <div>
+        <div className="mt-14">
           <div>
             <div>
-              <p className="bg-red-600 w-[2rem] h-1 t absolute  left-96 mt-4 ml-64"></p>
-              <h5 className="mt-28 relative -ml-8 text-center text-2xl font-bold">
-                Təmir
-              </h5>
-              <p className="bg-red-600 w-[2rem] h-1"></p>
-            </div>
-
-            <div className="">
-              <span className=" flex justify-center items-center gap-10 flex-wrap ">
-                <span className=" max-sm:-mt-6">
-                  <IoIosArrowBack />
-                </span>
-
-                {/* 
-<span > <Image src="/Group1.png" alt='beko' width={245} height={291}/></span>
-<span > <Image src="/Group3.png" alt='riffel' width={245} height={291}/>
-<span> <Image className='-translate-y-64 translate-x-16 mt-4 bg-slate-50' src="/Soyuducu.png" alt='soyuducu' width={114} height={26}/></span>
- <span><Image className='-translate-y-64 ml-12 mt-1 ' src="/soyud.png" alt='soyud' width={148} height={144}/></span>
-
-</span>
-
-<span > <Image src="/Group3.png" alt='riffel' width={245} height={291}/></span>
-<span > <Image src="/Group3.png" alt='riffel' width={245} height={291}/>
-<span><Image className='-translate-y-64 translate-x-16 mt-4 bg-slate-50' src="/qabyuyan.png" alt='qabyuyan' width={114} height={26}/></span>
-<span><Image className='-translate-y-56 ml-11 -mt-7 ' src="/paltar.png" alt='paltar' width={140} height={144}/></span>
-
-</span> */}
-
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-
-                <span className=" max-sm:-mt-6">
-                  <IoIosArrowForward />
-                </span>
-              </span>
-            </div>
-            <div>
-              <Link href={"#"}>Hamisina bax</Link>
+              <h1 className=" text-center font-bold text-xl max-sm:w-72 max-sm:m-auto">
+                Bütün brendlərin məişət texnikasını təmir edirik
+              </h1>
             </div>
           </div>
 
-          {/* <div className=" ">
-            <div className="m-auto ml-8 ">
-              <p className="bg-red-600 w-[2rem] h-1 t absolute  left-64 ml-80"></p>
-              <h5 className=" relative -ml-16 text-center text-2xl font-bold">
-                Quraşdırılma
-              </h5>
-              <p className="bg-red-600 w-[2rem] h-1   ml-96  "></p>
-            </div>
-
-            <div>
-              <span className=" flex justify-center gap-10 ">
-                <span className=" max-sm:-mt-6">
-                  <IoIosArrowBack />
-                </span>
-
-                <span>
-                  <Image
-                    src="/Group3.png"
-                    alt="riffel"
-                    width={245}
-                    height={291}
-                  />
-                </span>
-
-                <span>
-                  <Image
-                    src="/Group3.png"
-                    alt="riffel"
-                    width={245}
-                    height={291}
-                  />
-
-                  <span>
-                    <Image
-                      className=" bg-slate-50"
-                      src="/qabyuyan.png"
-                      alt="qabyuyan"
-                      width={114}
-                      height={26}
-                    />
-                  </span>
-                  <span>
-                    <Image
-                      className=" ml-11"
-                      src="/paltar.png"
-                      alt="paltar"
-                      width={140}
-                      height={144}
-                    />
-                  </span>
-                </span>
-
-                <span>
-                  <span>
-                    <Image
-                      src="/Group3.png"
-                      alt="riffel"
-                      width={245}
-                      height={291}
-                    />
-                  </span>
-                  <span>
-                    <Image
-                      className="  bg-slate-50"
-                      src="/elektriksobasi.png"
-                      alt="elektriksobasi"
-                      width={120}
-                      height={26}
-                    />
-                  </span>
-                  <span>
-                    <Image
-                      className=" ml-14"
-                      src="/elektrik.png"
-                      alt="riffel"
-                      width={148}
-                      height={144}
-                    />
-                  </span>
-                </span>
-                <span>
-                  <Image
-                    src="/Group1.png"
-                    alt="beko"
-                    width={245}
-                    height={291}
-                  />
-                </span>
-
-                <span className="max-sm:-mt-6">
-                  <IoIosArrowForward />
-                </span>
+          <div>
+            <span className="flex  justify-center gap-10 mt-10">
+              <span className="mt-6 max-sm:-mt-6">
+                <IoIosArrowBack />
               </span>
-              <div>
+
+              <span>
+                <Image src="/beko.png" alt="beko" width={83} height={65} />
+              </span>
+              <span>
                 <Image
-                  className="m-auto "
-                  src="/kecidhamisi.png"
-                  alt="kecid"
-                  width={248}
-                  height={60}
+                  src="/blomberg.png"
+                  alt="blomberg"
+                  width={144}
+                  height={65}
                 />
+              </span>
+              <span>
+                <Image
+                  src="/grundig.png"
+                  alt="grundig"
+                  width={182}
+                  height={65}
+                />
+              </span>
+              <span>
+                <Image src="/riffel.png" alt="riffel" width={126} height={65} />
+              </span>
+              <span className="mt-1.5 ">
+                <Image src="/lg.png" alt="lg" width={72} height={65} />
+              </span>
+              <span>
+                <Image
+                  src="/samsung.png"
+                  alt="samsung"
+                  width={146}
+                  height={65}
+                />
+              </span>
+
+              <span className=" mt-6 max-sm:-mt-6">
+                <IoIosArrowForward />
+              </span>
+            </span>
+          </div>
+        </div>
+        {/*  */}
+        {/*  */}
+        <div className='bg-[url("/bg.png")] w-full h-[60rem] mb-10'>
+          <div>
+            <div>
+              <div className="">
+                {/* <p className=" bg-red-600  w-16 h-2  "></p> */}
+                <h5 className="p-2 mb-10 relative -ml-8 text-center text-2xl font-bold ">
+                  Təmir
+                </h5>
+                {/* <p className="  bg-red-600  w-16 h-2 absolute right-96 -mt-12  mr-56 "></p> */}
+              </div>
+
+              <div className="">
+                <span className=" flex justify-center items-center gap-10 flex-wrap ">
+                  <span className=" max-sm:-mt-6">
+                    <IoIosArrowBack />
+                  </span>
+                  <div>
+                    <Card />
+                  </div>
+
+                  <span className=" max-sm:-mt-6">
+                    <IoIosArrowForward />
+                  </span>
+                </span>
+                <div>
+                  <Link
+                    className=" flex  justify-center items-center mt-10 m-auto border-2 rounded-md border-sky-700 w-40 h-10 p-1  text-sky-700"
+                    href={"#"}
+                  >
+                    Hamisina bax <FaArrowRight className="pl-1" />
+                  </Link>
+                </div>
               </div>
             </div>
-          </div> */}
+            <span className=" flex justify-center items-center gap-10 flex-wrap ">
+                  <span className=" max-sm:-mt-6">
+                    <IoIosArrowBack />
+                  </span>
+                  <div>
+                    <Card2 />
+                  </div>
+
+                  <span className=" max-sm:-mt-6">
+                    <IoIosArrowForward />
+                  </span>
+                </span>
+                <div>
+                  <Link
+                    className=" flex  justify-center items-center mt-10 m-auto border-2 rounded-md border-sky-700 w-40 h-10 p-1  text-sky-700"
+                    href={"#"}
+                  >
+                    Hamisina bax <FaArrowRight className="pl-1" />
+                  </Link>
+                </div>
+          </div>
+        </div>
+
+        <div className="flex justify-center m-16  gap-28  max-sm:mb-14 max-sm:p-5">
+          <span>
+            <Image
+              className="bg-slate-300 shadow-md rounded-md p-3 w-32 h-16"
+              src="/beko.png"
+              alt="beko"
+              width={83}
+              height={65}
+            />
+          </span>
+          <span>
+            <Image
+              className="bg-slate-300 shadow-md rounded-md p-3 w-32 h-16"
+              src="/blomberg.png"
+              alt="blomberg"
+              width={144}
+              height={65}
+            />
+          </span>
+          <span>
+            <Image
+              className="bg-slate-300 shadow-md rounded-md p-3 w-32 h-16"
+              src="/grundig.png"
+              alt="grundig"
+              width={182}
+              height={65}
+            />
+          </span>
+          <span>
+            <Image
+              className="bg-slate-300 shadow-md rounded-md p-3 w-32 h-16"
+              src="/riffel.png"
+              alt="riffel"
+              width={126}
+              height={65}
+            />
+          </span>
         </div>
       </div>
-
-      {/* <div className="flex justify-center  gap-28 ">
-        <span>
-          <Image
-            className="bg-slate-300 shadow-md rounded-md p-3 w-32 h-16"
-            src="/beko.png"
-            alt="beko"
-            width={83}
-            height={65}
-          />
-        </span>
-        <span>
-          <Image
-            className="bg-slate-300 shadow-md rounded-md p-3 w-32 h-16"
-            src="/blomberg.png"
-            alt="blomberg"
-            width={144}
-            height={65}
-          />
-        </span>
-        <span>
-          <Image
-            className="bg-slate-300 shadow-md rounded-md p-3 w-32 h-16"
-            src="/grundig.png"
-            alt="grundig"
-            width={182}
-            height={65}
-          />
-        </span>
-        <span>
-          <Image
-            className="bg-slate-300 shadow-md rounded-md p-3 w-32 h-16"
-            src="/riffel.png"
-            alt="riffel"
-            width={126}
-            height={65}
-          />
-        </span>
-      </div> */}
     </div>
   );
 };
--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -4,20 +4,64 @@ import { FaArrowRight } from "react-icons/fa6";
 
 const Card = () => {
   return (
-    <div className=" bg-slate-300 flex flex-col items-center">
-      <h2>KONDİSİONER</h2>
-      <Image src="/kondisioner.png" alt="beko" width={245} height={291} />
+  <div>
+ <div className="flex justify-center items-center gap-5 mt-14   max-sm:flex-wrap ">
+ <div className=" bg-slate-200 w-60 h-64 flex flex-col relative items-center ">
+      <h2 className="mt-5 text-xl">KONDİSİONER</h2>
+      <Image className="mt-5 " src="/kondisioner.png" alt="kondisioner" width={200} height={150} />
       <Link
-        className="w-[80px] h-[40px]  rounded-tl-[80px] rounded-tr-[80px] bg-blue-600 flex justify-center items-center  "
+        className="w-[80px] h-[40px]  rounded-tl-[80px] rounded-tr-[80px] bg-blue-600 absolute bottom-0 flex justify-center items-center  "
         href={"#"}
       >
-        <FaArrowRight />
+        <FaArrowRight className="text-slate-50" />
       </Link>
     </div>
+
+    <div className=" bg-slate-200 w-60 h-64  flex flex-col relative items-center">
+      <h2 className="mt-5 text-xl">Soyuducu</h2>
+      <Image className="mt-5 "  src="/soyud.png" alt="soyud" width={145} height={144} />
+      <Link
+        className="w-[80px] h-[40px]  rounded-tl-[80px] rounded-tr-[80px] bg-slate-100 absolute bottom-0  flex justify-center items-center  "
+        href={"#"}
+      >
+        <FaArrowRight  className='text-blue-500' />
+      </Link>
+    </div>
+
+
+
+    <div className=" bg-slate-200 w-60 h-64  flex flex-col relative items-center">
+      <h2 className="mt-5 text-xl">Paltaryuyan</h2>
+      <Image className="mt-5"  src="/paltaryuyan.png" alt="paltaryuyan" width={150} height={157} />
+      <Link
+        className="w-[80px] h-[40px]  rounded-tl-[80px] rounded-tr-[80px] bg-slate-100 absolute bottom-0  flex justify-center items-center  "
+        href={"#"}
+      >
+        <FaArrowRight  className='text-blue-500' />
+      </Link>
+    </div>
+
+    <div className=" bg-slate-200 w-60 h-64  flex flex-col relative items-center">
+      <h2 className="mt-5 text-xl">QABYUYAN</h2>
+      <Image className="mt-5"  src="/qabyuyan.png" alt="qabyuyan" width={140} height={152} />
+      <Link
+        className="w-[80px] h-[40px]  rounded-tl-[80px] rounded-tr-[80px] bg-slate-100 absolute bottom-0  flex justify-center items-center  "
+        href={"#"}
+      >
+        <FaArrowRight  className='text-blue-500' />
+      </Link>
+    </div>
+
+
+   </div>
+
+
+
+
+  </div>
   );
 };
 
 export default Card;
 
-// background: url(<path-to-image>), lightgray 50% / cover no-repeat;
-// mix-blend-mode: multiply;
+
