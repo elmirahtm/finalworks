8b748ff79ff00b1ca79475a3cc59bf6d6159dd21
elmirahtm <elmirahatamova28@gmail.com>
2024-02-21 01:44:17 +0400

    page added

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -34,10 +34,10 @@ const Haqqımızda = () => {
       <div>
         <div className='bg-[url("/bg.png")] w-full -mt-28 h-[209rem] mb-10 max-sm:h-[240rem]'>
         <div>
-          <div className="container m-auto">
-            <div className="bg-white w-[1060px] h-[1140px] max-sm:h-[1190px] mb-20 -mt-10  m-auto shadow-xl p-5 max-sm:mb-20 max-sm:w-[30rem] max-sm:m-auto max-sm:pt-3">
+          <div className="container m-auto max-sm:ml-0 ">
+            <div className="bg-white w-[1060px] h-[1140px] max-sm:h-[1215px] mb-20 -mt-10  m-auto shadow-xl p-5 max-sm:mb-20 max-sm:w-[30rem] max-sm:m-auto max-sm:pt-3">
               <span >
-             <video  className="m-auto " width={"944"} height={"190"} controls >
+             <video  className="m-auto max-sm:w-[350px] " width={"944"} height={"190"} controls >
 <source  src="/videomaster.mp4" type="video/mp4"/>
               
                 {" "}
@@ -108,7 +108,7 @@ const Haqqımızda = () => {
                       <h4 className="text-xl mt-3 font-bold  max-sm:ml-24">
                         Zəmanət qüvvəsi
                       </h4>
-                      <p className="w-[30rem] max-sm:w-[22rem] max-sm:text-justify max-sm:ml-8 ">
+                      <p className="w-[30rem] max-sm:w-[20rem] max-sm:text-justify max-sm:ml-8 ">
                         Soliton mağazalar şəbəkəsi öz məhsullarına yüksək
                         keyfiyyət verir və bütün məhsullarına 1 ildən 3 ilədək
                         zəmanət təklif edir. Biz sizə pulsuz çatdırılma və
@@ -121,21 +121,21 @@ const Haqqımızda = () => {
               </div>
             </div>
           </div>
-          <div className="container m-auto ">
-            <div className="flex justify-center max-sm:block max-sm:bg-white max-sm:h-[110rem] ">
-              <div className="bg-white w-[70rem] mb-20 shadow-xl py-10 ">
+          <div className="container  max-sm:ml-0  ">
+            <div className="flex justify-center max-sm:block max-sm:bg-white max-sm:h-[110rem] max-sm:ml-0">
+              <div className="bg-white w-[70rem] max-sm:w-[30rem] mb-20 shadow-xl py-10 m-auto max-sm:-ml-24  ">
                 <div className="px-32">
                   <div>
-                    <h1 className="font-bold text-2xl ml-20">Servis Mərkəzi</h1>
-                    <h5 className="font-bold mx-16 my-8 max-sm:w-[26rem]">
+                    <h1 className="font-bold text-2xl ml-20 max-sm:ml-0 ">Servis Mərkəzi</h1>
+                    <h5 className="font-bold mx-16 max-sm:mx-0 my-8 max-sm:w-[20rem]">
                       Bu gün bizim Call Center bir neçə funksiya həyata keçirir:
                     </h5>
                   </div>
-                  <ul className="ml-16">
+                  <ul className="ml-16  max-sm:ml-0 ">
                     <li className="mb-7">
                       Korporativ daxili zənglərin yönəldilməsi.
                     </li>
-                    <li className="mb-7 max-sm:w-[26rem]">
+                    <li className="mb-7 w-[26rem] max-sm:w-[23rem]">
                       Məlumat-soraq dəstəyi, yeni müştərilərə mövcud olan məhsul
                       xətti üzrə məsləhət verilməsi.
                     </li>
@@ -144,12 +144,12 @@ const Haqqımızda = () => {
                     </li>
                   </ul>
                   <div>
-                    <h3 className="text-3xl w-[50rem] ml-14 max-sm:w-[26rem]">
+                    <h3 className="text-3xl w-[30rem] ml-14 max-sm:ml-0  max-sm:w-[26rem]">
                       Call Center-də bu mövzular haqqında ətraflı məlumat əldə
                       etmək mümkündür.
                     </h3>
                   </div>
-                  <ul className="ml-16">
+                  <ul className="ml-16 max-sm:ml-0">
                     <li className="mt-9 mb-7">Servislərin ünvanları</li>
                     <li className="mb-7">
                       Keçirilən kompaniya və aksiyalar, onların şərtləri
@@ -160,7 +160,7 @@ const Haqqımızda = () => {
                     </li>
                   </ul>
                   <div>
-                    <p className="ml-16 w-[40rem] max-sm:w-[26rem] ">
+                    <p className="ml-16 max-sm:ml-0 w-[40rem] max-sm:w-[22rem] ">
                       Call Center-dən alınmlş məlumatların əsasında hesabatlar
                       hazırlanır, bu hesabatlar sonradan kompaniyanın rəqabət
                       qabiliyyətini dəyərlindirməyə və lazım olduqda məhsul
@@ -170,24 +170,24 @@ const Haqqımızda = () => {
                       kompleks şəkildə gün ərzində yüzlərlə zənglərin qəbul
                       edilməsinə və işin rahat görülməsinə imkan yaradır.
                     </p>
-                    <p className="mt-10 ml-16 w-[35rem] max-sm:w-[26rem]">
+                    <p className="mt-10 ml-16 max-sm:ml-0 w-[35rem] max-sm:w-[26rem]">
                       "Soliton Service" Call Center-i həftənin bütün günləri
                       saat 09:30 - 18:00 kimi fəaliyyət göstərir.
                     </p>
                   </div>
                 </div>
               </div>
-              <div className="relative">
-                <div className="border-2 border-blue-300 absolute right-32 top-12 w-[370px] h-[438px] max-sm:h-[538px] max-sm:left-12 max-sm:-mt-20 "></div>
-                <span>
+              <div className="relative  ">
+                <div className="border-2 border-blue-300 absolute right-32 top-12 w-[350px] max-sm:ml-4 max-sm:w-[320px] h-[438px] max-sm:h-[438px] max-sm:left-12 max-sm:-mt-20 "></div>
+                <div>
                   <Image
-                    className="mt-20 -ml-24 "
+                    className="mt-20 -ml-24 max-sm:ml-4"
                     src="/qulaqliq.png"
                     alt="qulaqliq"
                     width={459}
                     height={542}
                   />
-                </span>
+                </div>
               </div>
             </div>
           </div>
