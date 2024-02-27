75470153516e45277f4ecc7724cd54b8d21bf2f0
elmirahtm <elmirahatamova28@gmail.com>
2024-02-27 16:44:15 +0400

    responsive page

--- app/(site)/order/page.tsx
+++ app/(site)/order/page.tsx
@@ -1,19 +1,19 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { FaArrowLeftLong } from 'react-icons/fa6'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FaArrowLeftLong } from "react-icons/fa6";
 
 const Order = () => {
   return (
     <div>
-          <div className="flex max-sm:block max-sm:-mt-20">
-      <div className='bg-[url("/goy.jpg")] bg-no-repeat w-[40rem] h-[49rem]'>
+      <div className="flex max-sm:flex-col">
+      <div className="bg-sky-600  w-[800px] max-sm:w-[480px] h-[695px] ">
         <Link href="/">
-          <div className="flex text-white gap-4 ml-36 mt-20">
+          <div className="flex text-white gap-4 rounded-md ml-36 mt-10">
             <span>
-              <FaArrowLeftLong className="mt-1" />
+              <FaArrowLeftLong className="mt-1 max-sm:mt-6" />
             </span>
-            <p>Geri qayıt</p>
+            <p className="max-sm:mt-5">Geri qayıt</p>
           </div>
         </Link>
         <div>
@@ -27,26 +27,26 @@ const Order = () => {
         </div>
       </div>
 
-      <div className="bg-slate-200  w-[69rem] h-[49rem] max-sm:-mt-52 max-sm:pt-4">
-        <div className="text-center mt-52 ml-44 pt-20  bg-white w-[32rem] h-[20rem] max-sm:ml-16">
-          <span>
-            <Image
-              className="m-auto"
-              src="/stroke.png"
-              alt="stroke"
-              width={73}
-              height={73}
-            />
-          </span>
-          <h1 className="font-bold text-2xl p-4">
-            Sifarişiniz Yerinə Yetirildi!
-          </h1>
-          <p>Ən qısa zamanda sizinlə əlaqə saxlayacayıq.</p>
+        <div className="bg-slate-200 w-[1000px] max-sm:w-[480px] h-[695px]">
+          <div className="text-center mt-52 ml-44 pt-20 max-sm:-ml-6 ">
+            <span>
+              <Image
+                className="m-auto"
+                src="/stroke.png"
+                alt="stroke"
+                width={73}
+                height={73}
+              />
+            </span>
+            <h1 className="font-bold text-2xl p-4">
+              Sifarişiniz Yerinə Yetirildi!
+            </h1>
+            <p>Ən qısa zamanda sizinlə əlaqə saxlayacayıq.</p>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Order
+export default Order;
